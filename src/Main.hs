8273ce5428aebdd05e275459249bfcb3f4c8b1bd
Saurabh Nanda <saurabh@vacationlabs.com>
2019-07-25 20:06:47 +0530

    added comments

--- src/Main.hs
+++ src/Main.hs
@@ -18,6 +18,16 @@ import Control.Exception.Lifted as E
 import Data.Text
 
 
+--
+-- First, create a Postgres user and DB locally:
+--
+-- $ createuser -d -l -P hedgehog
+-- $ createdb -U hedgehog -W hedgehog
+--
+
+
+-- | Hard-coded DB credentials. If you've setup your local DB differently, then
+-- you will have to change these
 dbCredentials :: PGS.ConnectInfo
 dbCredentials = ConnectInfo
   { connectHost = "localhost"
@@ -27,21 +37,51 @@ dbCredentials = ConnectInfo
   , connectDatabase = "hedgehog"
   }
 
+
+-- | Before attemptimg to run the tests, you will need to run this function in a
+-- GHCi session
+createTables :: IO ()
+createTables = do
+  conn <- PGS.connect dbCredentials
+  C.finally
+    (void $ PGS.execute_ conn $
+      "create table users(id serial primary key, name text not null, email text not null);" <>
+      "create table posts(id serial primary key, user_id integer not null references users(id), title text not null, body text not null);")
+    (PGS.close conn)
+
+
+-- | The app's environment consists of a DB connection, NOT the entire DB pool.
+-- All the DB operations that the app does, need to happen through this single
+-- DB connection.
 data Env = Env
   { envDbConnection :: Connection
   }
 
-newtype TestM a = TestM (ReaderT Env IO a) deriving (MonadIO, Monad, Functor, Applicative, MonadMask, MonadThrow, MonadUnliftIO, MonadCatch, MonadReader Env)
 
+-- | All the app code is polymorphic in `m` with a bunch of typeclass
+-- constraints. This is one of the important constraints.
 class (MonadIO m) => HasDatabase m where
   getDbConnection :: m Connection
 
 instance HasDatabase TestM where
   getDbConnection = envDbConnection <$> ask
 
+
+-- | The app monad, which is called TestM in this case, but it will implement
+-- the HasDatabase type-class below
+newtype TestM a = TestM (ReaderT Env IO a)
+  deriving (MonadIO, Monad, Functor, Applicative, MonadMask, MonadThrow, MonadUnliftIO, MonadCatch, MonadReader Env)
+
+-- | Helper to unwrap the `TestM` newtype and run the ReaderT action embedded
+-- inside it. I prefer flipping the order of arguments around.
 runTestM :: Env -> TestM a -> IO a
 runTestM env (TestM action) = runReaderT action env
 
+
+-- | Helper function to create a top-level DB pool so that properties can be
+-- tested in parallel. Each run/invocation of the property _should_ end-up
+-- getting a different connection from this pool, so that they are isolated from
+-- each other.
 withPool :: (Pool Connection -> IO ()) -> IO ()
 withPool action = do
   pool <- Pool.createPool
@@ -52,7 +92,10 @@ withPool action = do
     10                          -- number of connections per stripe
   C.finally (action pool) (Pool.destroyAllResources pool)
 
-
+-- | Very important function, which prepares the environment a `TestM` action
+-- (basically takes a new connection from the DB pool), runs the action with
+-- that DB connectiom, and ROLLS IT BACK, so that the DB is left in a pristine
+-- state for subsequent tests/properties.
 withRollback :: Pool Connection -> TestM a -> IO a
 withRollback pool action = Pool.withResource pool $ \conn ->
     let env = Env { envDbConnection = conn }
@@ -63,19 +106,26 @@ withRollback pool action = Pool.withResource pool $ \conn ->
                             )
                             (liftIO $ PGS.rollback conn)
 
-
+-- | A polymorphic record to represent a User. This is the style that Opaleye
+-- (or Beam) uses.
 data UserPoly a = UserPoly
   { userId :: a
   , userName :: Text
   , userEmail :: Text
   } deriving (Eq, Show)
 
+-- | A new user where the `id` (primary-key) has no value
 type NewUser = UserPoly ()
+
+-- | A user saved to the DB, where the `id` is an Int provided by the DB itself.
 type User = UserPoly Int
 
+-- | PG-simple boilerplate
 instance FromRow User where
   fromRow = UserPoly <$> PGS.field <*> PGS.field <*> PGS.field
 
+-- | Function to create a user in any monad that implements the HasDatabase
+-- type-class. Most of the app code is written in this style.
 createUser :: (HasDatabase m) => NewUser -> m User
 createUser UserPoly{userName, userEmail} = do
   conn <- getDbConnection
@@ -84,18 +134,27 @@ createUser UserPoly{userName, userEmail} = do
     [r] -> pure r
     x -> Prelude.error $ "Not exepcting insert...returning to result in multiple rows: " <> show x
 
+
+-- | Post created by a user. Here I am using the Persistent way of doing things,
+-- where the id/primary-key is kep separate from the core value. This is just
+-- for demonstration; my codebase uses the Opaleye/Beam style demonstrated with
+-- the `User` type above. The `Post` type represents an unsaved post.
 data Post = Post
   { postUserId :: Int
   , postTitle :: Text
   , postBody :: Text
   } deriving (Eq, Show)
 
+-- | A newtype which represents a Post that has been saved to the DB. It
+-- basically types the id/primary-key along with the Post value.
 newtype PostEntity = PostEntity (Int, Post) deriving (Eq, Show)
 
+-- | Pg-simple boilerplate
 instance FromRow PostEntity where
   fromRow = PostEntity <$> ((,) <$> PGS.field <*> (Post <$> PGS.field <*> PGS.field <*> PGS.field))
 
-
+-- | Function to create a post in any monad that implements the HasDatabase
+-- type-class. Most of the app code is written in this style.
 createPost :: (HasDatabase m) => Post -> m PostEntity
 createPost Post{postUserId, postTitle, postBody} = do
   conn <- getDbConnection
@@ -105,21 +164,33 @@ createPost Post{postUserId, postTitle, postBody} = do
     x -> Prelude.error $ "Not exepcting insert...returning to result in multiple rows: " <> show x
 
 
+-- | Function to generate a random UNSAVED user.
 randomUser :: (MonadGen m) => m NewUser
 randomUser = UserPoly <$> (pure ()) <*> (Gen.text (Range.constant 1 100) Gen.unicodeAll) <*> (Gen.text (Range.constant 1 100) Gen.unicodeAll)
 
+-- | Function to generate a randomg UNSAVED post. However, please note, that
+-- this requires a saved User to be passed-in because `postUserId` is a
+-- required, non-nullable field.
 randomPost :: (MonadGen m) => User -> m Post
 randomPost UserPoly{userId} = Post <$> (pure userId) <*> (Gen.text (Range.constant 1 100) Gen.unicodeAll) <*> (Gen.text (Range.constant 1 100) Gen.unicodeAll)
 
 
+-- | This is the kind of test I am trying to write, but I have commented it out,
+-- because it is not compiling. The uncommented code below is just boilerplate
+-- to get the entire file to compile.
+--
+-- myProperty :: Pool Connection -> TestTree
+-- myProperty pool = testProperty "My property" $ property $ withRollback pool $ do
+--   newuser <- forAll randomUser
+--   user <- createUser newuser
+--   newpost <- forAll $ randomgPost user
+--   post <- createPost newpost
+--   True === True
 myProperty :: Pool Connection -> TestTree
-myProperty pool = testProperty "My property" $ property $ withRollback pool $ do
-  newuser <- forAll randomUser
-  user <- createUser newuser
-  newpost <- forAll $ randomgPost user
-  post <- createPost newpost
-  True === True
+myProperty pool = testProperty "My property" $ property $ pure ()
 
+
+-- | main-related boilerplate. 
 main :: IO ()
 main = withPool $ \pool -> defaultMain $ testGroup "All tests" [myProperty pool]
 
